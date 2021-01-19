a45138d478d619ad17024b0f76d7a4cf0d0e4d27
SebasBar <toolt2y@gmail.com>
2021-01-19 16:47:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const client = require("./config/db");
+const mainRouter = require("./routes");
+const errorMiddleware = require("./middleware/error-handling.middleware");
+const error = require("http-errors");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(mainRouter);
+
+app.use("*", (req, res, next) => {
+  next(error(404, "Not found"));
+});
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`app is running on ${PORT}`);
+});
+
+module.exports = app;
