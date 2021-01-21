421a6f3a4cdd7baa1f6038b2be127a4c321cbf13
Duran Sofia <durannsofia@gmail.com>
2021-01-21 20:11:23 +0100

    added hobby and skill

--- controller/employee.controller.js
+++ controller/employee.controller.js
@@ -98,3 +98,152 @@ exports.deleteEmployee = async (req, res, next) => {
     next(err);
   }
 };
+
+// add SKILL to employee
+
+const findEmployee = async (id) => {
+  const employee = await client.employee.findUnique({ where: { id } });
+  return employee;
+};
+const findSkill = async (id) => {
+  const skill = await client.skill.findUnique({ where: { id } });
+  return skill;
+};
+
+exports.createSkill = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const { name, type } = req.body;
+    const employee = await findEmployee(employeeId);
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    const createdSkill = await client.skill.create({
+      data: {
+        name: name,
+        type: type,
+        employee: { connect: { id: employeeId } },
+      },
+    });
+    res.status(200).json(createdSkill);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//{ pass this in postman
+//   "skillId": 11
+// }
+
+exports.updateSkill = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const employee = await findEmployee(employeeId);
+
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    const skillId = req.body.skillId;
+    const updatedUser = await client.employee.update({
+      where: { id: employeeId },
+      data: { skill: { connect: { id: skillId } } },
+      include: { skill: true },
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.removeSkill = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const skillId = Number(req.params.skillId);
+    const employee = await findEmployee(employeeId);
+    const skill = await findSkill(skillId);
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    if (!skill) {
+      throw createError(404, "Skill not Found");
+    }
+    const removedSkill = await client.employee.update({
+      where: { id: employeeId },
+      data: { skill: { disconnect: { id: skillId } } },
+      include: { skill: true },
+    });
+    res.status(200).json(removedSkill);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// add HOBBY to employee
+const findHobby = async (id) => {
+  const hobby = await client.hobby.findUnique({ where: { id } });
+  return hobby;
+};
+
+exports.createHobby = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const { name, type } = req.body;
+    const employee = await findEmployee(employeeId);
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    const createdHobby = await client.hobby.create({
+      data: {
+        name: name,
+        type: type,
+        employee: { connect: { id: employeeId } },
+      },
+    });
+    res.status(200).json(createdHobby);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateHobby = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const employee = await findEmployee(employeeId);
+
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    const hobbyId = req.body.hobbyId;
+    const updatedUser = await client.employee.update({
+      where: { id: employeeId },
+      data: { hobby: { connect: { id: hobbyId } } },
+      include: { hobby: true },
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.removeHobby = async (req, res, next) => {
+  try {
+    const employeeId = Number(req.params.employeeId);
+    const hobbyId = Number(req.params.hobbyId);
+    const employee = await findEmployee(employeeId);
+    const hobby = await findHobby(hobbyId);
+    if (!employee) {
+      throw createError(404, "Employee not Found");
+    }
+    if (!hobby) {
+      throw createError(404, "Hobby not Found");
+    }
+    const removedHobby = await client.employee.update({
+      where: { id: employeeId },
+      data: { hobby: { disconnect: { id: hobbyId } } },
+      include: { hobby: true },
+    });
+    res.status(200).json(removedHobby);
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -4,6 +4,12 @@ const {
   updateEmployee,
   deleteEmployee,
   getOneEmployee,
+  createSkill,
+  updateSkill,
+  removeSkill,
+  createHobby,
+  updateHobby,
+  removeHobby,
 } = require("../controller/employee.controller");
 
 const router = require("express").Router();
@@ -13,5 +19,15 @@ router.get("/:employeeId", getOneEmployee);
 router.post("/", createEmployee);
 router.put("/:employeeId", updateEmployee);
 router.delete("/:employeeId", deleteEmployee);
+
+// employee skills router
+router.post("/:employeeId/skill", createSkill);
+router.put("/:employeeId/skill", updateSkill);
+router.delete("/:employeeId/skill/:skillId", removeSkill);
+
+// employee hobby router
+router.post("/:employeeId/hobby", createHobby);
+router.put("/:employeeId/hobby", updateHobby);
+router.delete("/:employeeId/hobby/:hobbyId", removeHobby);
 
 module.exports = router;
